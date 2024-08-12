3845407a272206ede1d5c7f9beaed3fd786bb229
Chouhan <kuldeep.chouhan@P3002383.local>
2024-08-12 15:16:34 +0530

    crud rest api

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ unique: true })
+  userName: string;
+}
